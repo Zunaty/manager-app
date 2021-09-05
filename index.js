d59ee23882d822011d9250a3730cf7f278a70f7a
Victor Perez <zunaty@gmail.com>
2021-09-05 00:15:18 -0600

    Ability to add department and role

--- index.js
+++ index.js
@@ -9,6 +9,37 @@ const db = mysql.createConnection({
     database: 'manager_db'
 });
 
+// Function to wait 1 sec
+const wait = () => {
+    setTimeout(()=>{}, 1000);
+};
+
+// Function to wait 1 sec and re prompt
+const waitPrompt = () => {
+    setTimeout(()=>{ promptUser() }, 1000);
+};
+
+// Function to view department table
+const depoView = () => {
+    db.query('SELECT department.name AS DepartmentName FROM department', (err, results) => {
+        console.table('Viewing Departments', results);
+    });
+};
+
+// Function to view roles table
+const rolesView = () => {
+    db.query('SELECT roles.title AS Title, roles.salary AS Salary, department.name AS Department FROM roles JOIN department ON roles.department_id = department.id;', (err, results) => {
+        console.table('Viewing Employee Roles', results);
+    });
+};
+
+// Function to view employee table
+const employeeView = () => {
+    db.query('SELECT employee.first_name AS FirstName, employee.last_name AS LastName, roles.title AS Title, roles.salary AS Salary, department.name AS DepartmentName FROM employee JOIN roles ON employee.role_id = roles.id JOIN department ON roles.department_id = department.id;', (err, results) => {
+        console.table('Viewing All Employees', results);
+    });
+};
+
 // Asking the user to select an option
 const promptUser = () => {
     return inquirer.prompt(
@@ -22,28 +53,104 @@ const promptUser = () => {
 
         // Grabbing Departments from the database to view
         if (listChoice === 'View all departments') {
-            db.query('SELECT name FROM department', function(err, results) {
-                console.table('Viewing Departments', results);
-            })
-            setTimeout(()=>{ promptUser() }, 1000)
+            depoView();
+            waitPrompt();
 
         // Grabbing Roles from the database to view
         } else if (listChoice === 'View all roles') {
-            db.query('SELECT title, salary FROM roles', function(err, results) {
-                console.table('Viewing Employee Roles', results);
-            })
-            setTimeout(()=>{ promptUser() }, 1000)
+            rolesView();
+            waitPrompt();
 
         // Grabbing Employees from the database to view
         } else if (listChoice === 'View all employees') {
-            db.query('SELECT employee.first_name AS first_name, employee.last_name AS last_name, roles.title AS title, roles.salary AS salary FROM employee JOIN roles ON employee.role_id = roles.id;', function(err, results) {
-                console.table('Viewing All Employees', results);
-            })
-            setTimeout(()=>{ promptUser() }, 1000)
+            employeeView();
+            waitPrompt();
+
+        // Adding a department to the department table
         } else if (listChoice === 'Add a department') {
+            return inquirer.prompt(
+                {
+                    type: 'input',
+                    message: 'What is your new department name?',
+                    name: 'depoName',
+                    validate: depoNameInput => {
+                        if(depoNameInput) {
+                            return true;
+                        } else {
+                            console.log('Please enter a department name!');
+                            return false;
+                        }
+                    }
+                }
 
+            // Taking the answer and inserting into table and then showing the change
+            ).then(({ depoName }) => {
+                db.query(`INSERT INTO department (name) VALUES ("${depoName}");`, (err, result) => {});
+
+                wait();
+
+                depoView();
+                waitPrompt();
+            });
+
+        // Adding a role to the roles table
         } else if (listChoice === 'Add a role') {
+            const roleChoices = [];
 
+            db.query('SELECT name FROM department', (err, results) => {
+                for(let i = 0; i < results.length; i++) {
+                    roleChoices.push(results[i].name);
+                };
+            });
+
+            wait();
+
+            return inquirer.prompt([
+                {
+                    type: 'input',
+                    message: 'What is your roles title?',
+                    name: 'roleTitle',
+                    validate: roleTitleInput => {
+                        if(roleTitleInput) {
+                            return true;
+                        } else {
+                            console.log('Please enter a roles title!');
+                            return false;
+                        }
+                    }
+                },
+                {
+                    type: 'input',
+                    message: 'What is your roles salary?',
+                    name: 'roleSalary',
+                    validate: roleSalaryInput => {
+                        if(roleSalaryInput) {
+                            return true;
+                        } else {
+                            console.log('Please enter a roles salary!');
+                            return false;
+                        }
+                    }
+                },
+                {
+                    type: 'list',
+                    message: 'Which department is this role in?',
+                    name: 'roleDepo',
+                    choices: roleChoices
+                }
+               
+            // Taking title, salary, and department id and inserting into roles table
+            ]).then((data) => {
+                const roleDepoID = roleChoices.indexOf(data.roleDepo, 0) + 1;
+                db.query(`INSERT INTO roles (title, salary, department_id) VALUES ("${data.roleTitle}", ${data.roleSalary}, ${roleDepoID});`, (err, result) => {});
+
+                wait();
+
+                rolesView();
+                waitPrompt();
+            });
+
+        
         } else if (listChoice === 'Add a employee') {
 
         } else if (listChoice === 'Update an employee role') {
